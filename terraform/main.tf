12db95d7d29d07cc16a2a965426b6650bee2ded6
stockviva-davidvv <david.yeung@moleculez.co>
2022-02-09 16:10:45 +0800

    feat: add buildx

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+locals {
+  name = "nodejs-infra"
+  environment = "dev"
+}
+
 terraform {
   required_providers {
     aws = {
@@ -14,18 +19,41 @@ terraform {
   }
 }
 provider "aws" {
-  profile = "david"
-  region  = "ap-southeast-1"
+  region = "ap-southeast-1"
 
   default_tags {
     tags = {
-      Environment = "dev"
-      Project     = "nodejs-infra"
+      Environment = local.environment
+      Project     = local.name
     }
   }
 }
 
 
 resource "aws_ecr_repository" "registry" {
-  name = "nodejs-infra"
+  name = local.name
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+
+  cidr = "10.1.0.0/16"
+
+  azs             = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets  = ["10.1.11.0/24", "10.1.12.0/24"]
+
+  enable_nat_gateway = false # false is just faster
+
+  tags = {
+    Environment = local.environment
+    Name        = local.name
+  }
 }
